ef450aae8bd248cb5865c2db807512d7cbbaeef6
Desktop <Desktop@git.com>
2024-05-05 23:47:52 +0800

    non

--- src/Basis/AcWing786.java
+++ src/Basis/AcWing786.java
@@ -4,10 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+//第k小
 public class AcWing786 {
     public static void main(String[] args) {
-
         try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
             String[] s = in.readLine().split(" ");
             int n = Integer.parseInt(s[0]);
