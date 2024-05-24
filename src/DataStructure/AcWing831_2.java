f255a296b101205a1325229a5ef8318b5fd25e31
laptop <laptop@git.com>
2024-05-24 10:13:48 +0800

    non

--- src/DataStructure/AcWing831_2.java
+++ src/DataStructure/AcWing831_2.java
@@ -10,8 +10,18 @@ public class AcWing831_2 {
             char[] p = (" " + in.readLine()).toCharArray();
             int m = Integer.parseInt(in.readLine());
             char[] s = (" " + in.readLine()).toCharArray();
-
-
+            long[] k = new long[m + 10], hs = new long[m + 10];
+            long hp = 0;
+            int K = 131;
+            k[0] = 1;
+            for (int i = 1; i <= m; i++) {
+                k[i] = k[i - 1] * K;
+                hs[i] = hs[i - 1] * K + s[i];
+                if (i <= n) hp = hp * K + p[i];
+            }
+            for (int i = 0; i <= m - n; i++) {
+                if ((hs[i + n] - hs[i] * k[n]) == hp) out.write(i + " ");
+            }
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
