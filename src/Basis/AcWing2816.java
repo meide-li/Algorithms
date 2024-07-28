a57e2134e61e1ae20401e6f39dc0b64cfb366663
laptop <laptop@git.com>
2024-07-28 15:57:01 +0800

    non

--- src/Basis/AcWing2816.java
+++ src/Basis/AcWing2816.java
@@ -1,36 +1,32 @@
 package Basis;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 // 判断子序列
 public class AcWing2816 {
-    public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
-            String[] s = in.readLine().split(" ");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
-            int[] a = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt).toArray();
-            int[] b = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt).toArray();
-            int i = 0;
-            for (int j = 0; j < m; j++) {
-                if (i < n && a[i] == b[j]) i++;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = in.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int[] a = Arrays.stream(in.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] b = Arrays.stream(in.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
 
-//            另一种写法
-//            int i = 0, j = 0;
-//            for (; i < n; i++, j++) {
-//                while (j < m && b[j] != a[i]) j++;
-//                if (j >= m) break;
-//            }
-
-            if (i == n) System.out.println("Yes");  //注意这里是 i==n而不是n-1，因为最后判断成功还会i++一次
-            else System.out.println("No");
-        } catch (IOException e) {
-            e.printStackTrace();
+        int i = 0;
+        for (int j = 0; j < m; j++) {
+            if (i < n && a[i] == b[j]) i++;
         }
+
+//        另一种写法
+//        int i = 0, j = 0;
+//        for (; i < n; i++, j++) {
+//            while (j < m && b[j] != a[i]) j++;
+//            if (j >= m) break;
+//        }
+
+        if (i == n) System.out.println("Yes");  //注意这里是 i==n而不是n-1，因为最后判断成功还会i++一次
+        else System.out.println("No");
     }
 }
--- src/Basis/AcWing800.java
+++ src/Basis/AcWing800.java
@@ -1,36 +1,30 @@
 package Basis;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
-// 数组元素的目标和——哈希表法
+// 数组元素的目标和——哈希法
 public class AcWing800 {
-    public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
-            String[] s = in.readLine().split(" ");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
-            int x = Integer.parseInt(s[2]);
-            int[] A = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt).toArray();
-            int[] B = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt).toArray();
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < n; i++) {
-                map.put(x - A[i], i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = in.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int x = Integer.parseInt(s[2]);
+        int[] A = Arrays.stream(in.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] B = Arrays.stream(in.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(x - A[i], i);
+        }
+        for (int i = 0; i < m; i++) {
+            if (map.containsKey(B[i])) {
+                System.out.println(map.get(B[i]) + " " + i);
+                break;
             }
-            for (int i = 0; i < m; i++) {
-                if (map.containsKey(B[i])) {
-                    System.out.println(map.get(B[i]) + " " + i);
-                    break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
 }
